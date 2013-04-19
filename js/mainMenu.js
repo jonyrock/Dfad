7bab681407b9374810a7c508305b9d2a1dd171c7
jonyrock <ilms@live.ru>
2013-04-19 17:14:15 +0400

    apatan setup and test formating, comment index delete

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -7,112 +7,104 @@ var submenuWidth = 0;
 var oldMenuData = "";
 var menuData = "";
 
-$(function () {
-    
-    $(".menu-option-text a").click(function () {
+$(function() {
+
+    $(".menu-option-text a").click(function() {
         var menuOptionHolder = $(this).parent().parent();
         var isComplex = menuOptionHolder.find(".sub-menu-holder").length > 0;
-        
+
         if (isComplex && window.location.hash.indexOf("index") == -1) {
             menuOptionHolder.find("a[href='#index.html']").trigger("click");
-        } 
+        }
 
-        
     });
 
 });
 
 function menuListeners() {
-    
+
     /* We add 2 px in order to fix the 2px margin on the right. Since sub menu holder */
     /* has overflow hidden the 2px will fill the gap in IE 8 and in the other browser it won't be shown. */
-    
-    function hideSubmenu(obj) { obj.css('opacity', '0').css('display', 'none'); }
+
+    function hideSubmenu(obj) {
+        obj.css('opacity', '0').css('display', 'none');
+    }
 
 
-    $(".menu-option-holder").click(
-        function (event) {
-            //alert("me click!");
-            event.preventDefault();
-            var idx = $(".menu-option-holder").index(this);
-            if (menuOptionID != idx && $(this).attr("data-module-type") != undefined && $(this).attr("data-module-type") != "#") {
-                if (loadedContent == false) return;
-                menuOptionID = idx;
-                submenuOptionID = -1;
+    $(".menu-option-holder").click(function(event) {
+        //alert("me click!");
+        event.preventDefault();
+        var idx = $(".menu-option-holder").index(this);
+        if (menuOptionID != idx && $(this).attr("data-module-type") != undefined && $(this).attr("data-module-type") != "#") {
+            if (loadedContent == false)
+                return;
+            menuOptionID = idx;
+            submenuOptionID = -1;
 
-                if (touchDevice) {
-                    menuOptionIn(menuOptionID, submenuOptionID);
-                }
-
-                var hashURL = "#" + menuOptionsArr[idx][1][2];
-                menuData = menuOptionsArr[idx][1];
-                $(window).unbind('hashchange', onTemplateHashChange);
-                window.location.hash = hashURL;
-                clearCustomInterval(delayInterval);
-                delayInterval = setInterval(function () {
-                    menuOptionClicked(menuData[2],
-                        menuData[0],
-                        menuData[1],
-                        menuData[3]);
-                    clearCustomInterval(delayInterval);
-                    $(window).bind('hashchange', onTemplateHashChange);
-                }, 400);
+            if (touchDevice) {
+                menuOptionIn(menuOptionID, submenuOptionID);
             }
-        }
-    );
 
+            var hashURL = "#" + menuOptionsArr[idx][1][2];
+            menuData = menuOptionsArr[idx][1];
+            $(window).unbind('hashchange', onTemplateHashChange);
+            window.location.hash = hashURL;
+            clearCustomInterval(delayInterval);
+            delayInterval = setInterval(function() {
+                menuOptionClicked(menuData[2], menuData[0], menuData[1], menuData[3]);
+                clearCustomInterval(delayInterval);
+                $(window).bind('hashchange', onTemplateHashChange);
+            }, 400);
+        }
+    });
 
     subCloseInterval = "";
-    $(".sub-menu-holder .sub-menu-option-holder").click(
-        function (event) {
-            event.preventDefault();
-            var submenuParent = $(this).parent().get(0);
-            var menuParent = $(submenuParent).parent().get(0);
-            currMenuOptionID = $(menuParent).index();
-            var submenuOptIdx = $(this).index();
-            if (submenuOptionID == submenuOptIdx && menuOptionID == currMenuOptionID) {
-                return false;
-            } else {
-                if (loadedContent == false) {
-                    return;
-                }
-                var subMenu = menuOptionsArr[currMenuOptionID][6];
-
-                if (touchDevice) {
-                    menuOptionIn(currMenuOptionID, submenuOptIdx);
-                    clearCustomInterval(subCloseInterval);
-                    subCloseInterval = setInterval(function () {
-                        touchContainer();
-                        clearCustomInterval(subCloseInterval);
-                    }, 200);
-
-                }
-
-                var disableIdx1 = undefined;
-                if (menuOptionID == currMenuOptionID) {
-                    disableIdx1 = true;
-                }
-                menuOptionID = currMenuOptionID;
-                submenuOptionID = submenuOptIdx;
-
-                var hashURL = subMenu[submenuOptIdx][0];
-
-                menuData = subMenu[submenuOptIdx][1];
-                $(window).unbind('hashchange', onTemplateHashChange);
-                window.location.hash = hashURL;
-
-                clearCustomInterval(delayInterval);
-                delayInterval = setInterval(function () {
-                    menuOptionClicked(menuData[2],
-                        menuData[0],
-                        menuData[1],
-                        menuData[3]);
-                    clearCustomInterval(delayInterval);
-                    $(window).bind('hashchange', onTemplateHashChange);
-                }, 400);
+    $(".sub-menu-holder .sub-menu-option-holder").click(function(event) {
+        event.preventDefault();
+        var submenuParent = $(this).parent().get(0);
+        var menuParent = $(submenuParent).parent().get(0);
+        currMenuOptionID = $(menuParent).index();
+        var submenuOptIdx = $(this).index();
+        if (submenuOptionID == submenuOptIdx && menuOptionID == currMenuOptionID) {
+            return false;
+        } else {
+            if (loadedContent == false) {
+                return;
             }
-            event.stopPropagation();
-        });
+            var subMenu = menuOptionsArr[currMenuOptionID][6];
+
+            if (touchDevice) {
+                menuOptionIn(currMenuOptionID, submenuOptIdx);
+                clearCustomInterval(subCloseInterval);
+                subCloseInterval = setInterval(function() {
+                    touchContainer();
+                    clearCustomInterval(subCloseInterval);
+                }, 200);
+
+            }
+
+            var disableIdx1 = undefined;
+            if (menuOptionID == currMenuOptionID) {
+                disableIdx1 = true;
+            }
+            menuOptionID = currMenuOptionID;
+            submenuOptionID = submenuOptIdx;
+
+            var hashURL = subMenu[submenuOptIdx][0];
+
+            menuData = subMenu[submenuOptIdx][1];
+            $(window).unbind('hashchange', onTemplateHashChange);
+            window.location.hash = hashURL;
+
+            clearCustomInterval(delayInterval);
+            delayInterval = setInterval(function() {
+                menuOptionClicked(menuData[2], menuData[0], menuData[1], menuData[3]);
+                clearCustomInterval(delayInterval);
+                $(window).bind('hashchange', onTemplateHashChange);
+            }, 400);
+        }
+        event.stopPropagation();
+    });
 }
 
 var currModuleType = '';
@@ -126,22 +118,16 @@ var submenuOptionID = -1;
 
 function storeMenuArr() {
     var extraWidth = $(".template-menu").width() + $("#menu-hider").width();
-    $("#template-menu").children().each(function (index, element) {
-        var menu = $(element),
-            subOptArr = "null",
-            subHol = "null",
-            menOptText = $(".menu-option-text a", menu);
+    $("#template-menu").children().each(function(index, element) {
+        var menu = $(element), subOptArr = "null", subHol = "null", menOptText = $(".menu-option-text a", menu);
         menuOptionsArr[index] = [];
         menuOptionsArr[index][0] = menu;
 
         if (menu.attr("data-module-type") == undefined) {
             var subMenu = [];
             subHol = menu.find(".sub-menu-holder");
-            subHol.children().each(function (index, elem) {
-                var submenu = $(elem),
-                    subOptTxt = $(".sub-menu-option-text a", this),
-                    subOptHref = String(subOptTxt.attr("href")),
-                    subOptHrefPath = subOptTxt.attr("data-path-href");
+            subHol.children().each(function(index, elem) {
+                var submenu = $(elem), subOptTxt = $(".sub-menu-option-text a", this), subOptHref = String(subOptTxt.attr("href")), subOptHrefPath = subOptTxt.attr("data-path-href");
                 subMenu[index] = [];
                 subMenu[index][0] = "#" + menOptText.text().toLowerCase() + "/" + subOptHref.replace("#", "");
                 subMenu[index][1] = [submenu.attr("data-module-type"), submenu.attr("data-side"), subOptHref.replace("#", ""), subOptHrefPath];
@@ -158,15 +144,20 @@ function storeMenuArr() {
         menuOptionsArr[index][4] = extraWidth - parseInt(menOptText.css("padding-left"), 10);
         menuOptionsArr[index][5] = subHol;
         menuOptionsArr[index][6] = subOptArr;
-        if (!touchDevice) menu.bind("mouseenter mouseleave", { idx: index }, menuOptionHover);
+        if (!touchDevice)
+            menu.bind("mouseenter mouseleave", {
+                idx : index
+            }, menuOptionHover);
     });
 }
 
 //TODO: imp init hover
-function menuOptionIn(idx1, idx2) {  }
+function menuOptionIn(idx1, idx2) {
+}
 
 //TODO: react when change hash
-function menuOptionOut(oldMenuID, oldSubID, disabMenu){}
+function menuOptionOut(oldMenuID, oldSubID, disabMenu) {
+}
 
 function setMenuData(val) {
     oldMenuData = val;
@@ -179,9 +170,12 @@ function setMenuData(val) {
     endPreviousModule = false;
 }
 
-function menuOptionHover(event) {}
+function menuOptionHover(event) {
+}
 
-function hideSubmenu(obj) { obj.css('opacity', '0').css('display', 'none'); }
+function hideSubmenu(obj) {
+    obj.css('opacity', '0').css('display', 'none');
+}
 
 /* select the current mobile option */
 
@@ -235,14 +229,26 @@ function menuOptionClicked(val, mType, sType, hrefPath) {
             loadedContent = true;
             var loadAnim = $("#loading-animation");
             if (loadAnim.length > 0) {
-                TweenMax.to(loadAnim, .3, { css: { right: "-104px" }, ease: Circ.easeOut });
+                TweenMax.to(loadAnim, .3, {
+                    css : {
+                        right : "-104px"
+                    },
+                    ease : Circ.easeOut
+                });
             }
             if (endModuleFunction == null) {
 
                 switch (menuData[0]) {
                     case "news":
                         var textPageInstanceHolder = $(txt_modCont);
-                        TweenMax.to(textPageInstanceHolder, .6, { css: { left: "0px" }, delay: 0.1, ease: Circ.easeInOut }); /*get_OffsetWidth() +*/
+                        TweenMax.to(textPageInstanceHolder, .6, {
+                            css : {
+                                left : "0px"
+                            },
+                            delay : 0.1,
+                            ease : Circ.easeInOut
+                        });
+                        /*get_OffsetWidth() +*/
                         endModuleFunction = endModuleTextPage;
                         moduleEnd = true;
                         break;
@@ -268,7 +274,12 @@ var touchMenuID = -1;
 function touchContainer() {
     touchRemoveOn = false;
     if (touchMenuID != -1 && menuOptionsArr[touchMenuID][6] != "null") {
-        var evt = { data: { idx: touchMenuID }, type: "mouseleave" };
+        var evt = {
+            data : {
+                idx : touchMenuID
+            },
+            type : "mouseleave"
+        };
         menuOptionHover(evt);
     }
     var moduleContainer = document.getElementById("module-container");
@@ -278,13 +289,8 @@ function touchContainer() {
 function updateMenu(currentURL, prevURL, sameURLParent, animate) {
     currentURL = currentURL.replace("#", "");
     prevURL = prevURL.replace("#", "");
-    
-    var returnURL = "",
-        i = 0,
-        j = 0,
-        tempMenuID = 0,
-        tempSubmID = 0,
-        idx = menuOptionsArr.length;
+
+    var returnURL = "", i = 0, j = 0, tempMenuID = 0, tempSubmID = 0, idx = menuOptionsArr.length;
 
     while (idx--) {
         if (menuOptionsArr[idx][1] != "null") {
