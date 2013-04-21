511dda8be8d9def1c35ff58c33d7886ba5147558
jonyrock <ilms@live.ru>
2013-04-21 20:28:16 +0400

    buggy animation imp

--- js/mainMenuView.js
+++ js/mainMenuView.js
@@ -2,6 +2,8 @@
 MainMenuView.menuHolder         = "#template-menu";
 MainMenuView.menuItemHolder     = "menu-option-holder";        // some class name
 MainMenuView.menuItemBackground = "menu-option-background";    // some class in div
+MainMenuView.menuAnimDuration   = 0.4;
+MainMenuView.menuAnimEase       = Quad.easeOut;
 
 
 function MainMenuView() {
@@ -9,21 +11,65 @@ function MainMenuView() {
     var me = this;
     
 	$(function() {
-	    me.items = $(MainMenuView.menuHolder).find("." + MainMenuView.menuItemHolder);
-        me.items.each(function() {
-            $(this).prepend("<div class='" + MainMenuView.menuItemBackground + "'></div>");
-            $(this).click(function (){ selectItem(this); });
-        });
-        
+	    me.menuHolderWidth = $(MainMenuView.menuHolder).width();
+	    buildItems();
+        if(!touchDevice)
+            initHoveringAnimation();
 	});
 	
-	function selectItem(item) {
-	    
+	function buildItems() {
+	    me.items = new Array();
+	    $(MainMenuView.menuHolder)
+	    .find("." + MainMenuView.menuItemHolder)
+	    .each(functon(i) {
+	        me.items[i] = new Object();
+	        me.items[i].holder = this;
+	        $(this).prepend("<div class='" + MainMenuView.menuItemBackground + "'></div>");
+	        me.items[i].back = $(this).find("." + MainMenuView.menuItemBackground);
+	        me.items[i].index = i;
+	        me.items[i].fixed = false;
+	    })
+	}
+	
+	function initHoveringAnimation() {
+	    for(var item in me.items) {
+	        $(item.holder).mouseenter(function() {
+	            if(item.fixed) return;
+                TweenMax.to(item.back, 
+                    MainMenuView.menuAnimDuration, 
+                    { css: { left: "0px"}, ease: MainMenuView.menuAnimEase });
+	       });
+	       $(item.holder).mouseleave(function() {
+                if(item.fixed) return;
+                TweenMax.to(item.back, 
+                    MainMenuView.menuAnimDuration, 
+                    { css: { left: me.menuHolderWidth + "px"}, ease: MainMenuView.menuAnimEase });
+        });
 	}
 	
 }
 
 MainMenuView.prototype.setActive = function (menuItemIndex) {
-    this.items.find("." + MainMenuView.menuItemBackground).hide();
-    this.items.eq(menuItemIndex).find("." + MainMenuView.menuItemBackground).show().css("left","0px");
+    for(var item in me.items) {
+        if(item.index == menuItemIndex) {
+            item.fixed = true;
+            if(!touchDevice) {
+                $(item.holder).trigger("mouseenter");
+            } else {
+                $(item.back).show().css("left","0px")
+            }
+        } else {
+            item.fixed = false;
+            if(!touchDevice)
+                $(item.holder).trigger("mouseleave");
+        }
+    });
 }
+
+
+
+
+
+
+
+
