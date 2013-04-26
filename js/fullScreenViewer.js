ac37d28b928b17b9cb8bbd29e4a3e42a50b4299c
jonyrock <ilms@live.ru>
2013-04-26 15:37:37 +0400

    separateviewer html

--- js/fullScreenViewer.js
+++ js/fullScreenViewer.js
@@ -5,22 +5,13 @@
  * @author: @jonyrock exclusively to dfad.com
  **/
 
-function fullScreenViewer(mediaItems, mediaItemsHtml, htmlTemplate) {
-
+function fullScreenViewer(mediaItems, mediaItemsHtml) {
     var me = this;
     this.mediaItems = mediaItems;
     this.mediaItemsHtml = mediaItemsHtml;
     this.isVisible = false;
     this.htmlMediaHolder = $("#full-width-preview-media-holder");
-    
-    this.buildDom = function() {
-        $("#template-wrapper").find("#full-width-preview").remove();
-        var res = '<div id="full-width-preview">' + htmlTemplate + '</div>'
-        $("#template-wrapper").append(res);
-        return $("#template-wrapper").find("#full-width-preview");
-    }
-    this.htmlHolder = this.buildDom();
-
+    this.htmlHolder = $("#template-wrapper").find("#full-width-preview");
 }
 
 fullScreenViewer.prototype.show = function() {
@@ -67,7 +58,7 @@ fullScreenViewer.buildFromHtml = function() {
     var templateHtml = $("#full-width-preview").html();
     $("#full-width-preview").remove();
 
-    return new fullScreenViewer(previewMediaArr, previewMediaDescArr, templateHtml);
+    return new fullScreenViewer(previewMediaArr, previewMediaDescArr);
 
 }
 
